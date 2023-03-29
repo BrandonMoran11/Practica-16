0fdccc632d3686290ccd3a190c9e77017161d185
BrandonMoran11 <bmoran2@ucol.mx>
2023-03-29 14:16:01 -0600

    Cambios al package.json y agregado del archivo style.css

--- src/server.js
+++ src/server.js
@@ -25,23 +25,13 @@ app.use('/', function (req,res,next ){
 });
 
 
-// app.get('/', function (req, res){
-//     res.render('index');
-// });
-// app.get('/student', function (req, res){
-//     res.render('student');
-// });
-// app.get('/person/:name', function(req, res){
-//     res.render('person', {NAME: req.params.name, Msg: req.query.msg, Times: req.query.times});
-// });
-
-app.post('/addStudent', function(req,res){
-    res.render('displayData', {nombre: req.body.nombre,
-                              edad: req.body.edad,
-                              nss: req.body.nss,
-                              tipoSangre: req.body.tipoSangre});
+// app.post('/addStudent', function(req,res){
+//     res.render('displayData', {nombre: req.body.nombre,
+//                               edad: req.body.edad,
+//                               nss: req.body.nss,
+//                               tipoSangre: req.body.tipoSangre});
               
-});
+// });
 
 app.listen(PORT,()=>{
     console.log('escuchando en el puerto 3000')
